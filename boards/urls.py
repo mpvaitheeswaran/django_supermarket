515f7ff9fe150060dd3d2fa884c09710ead406a1
mpvaitheeswaran <mpvaitheeswaran@gmail.com>
2021-10-19 11:34:54 +0530

    Kanban work done

--- boards/urls.py
+++ boards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.BoardList.as_view(),name='boards'),
     path('move/',views.moveProduct,name='update_kanban_column'),
+    path('add/',views.addProductBoard,name='add_product_board'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render,redirect
 from boards.models import Board
 from django.views.generic import ListView
-
+import json
 from products.models import Product
 
 # Create your views here.
@@ -13,6 +14,19 @@ class BoardList(ListView):
     model = Board
     template_name = 'boards/kanban_board.html'
     context_object_name = 'items'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context['products'] = Product.objects.filter(user=self.request.user)
+        boards = super().get_context_data(**kwargs)['items']
+        products = list(Product.objects.filter(user=self.request.user))
+        
+        for board in boards:
+            for product in products:
+                if board.product.id == product.id:
+                    #remove the curent product in products
+                    products.remove(product)
+        context['products'] = products    
+        return context
 
 def moveProduct(request):
     if request.method == 'POST':
@@ -22,3 +36,15 @@ def moveProduct(request):
         product.column = columnId
         product.save()
         return redirect('boards')
+
+def addProductBoard(request):
+    if request.method == 'GET' and request.is_ajax():
+        productIds = request.GET.getlist('selectedProIds[]')
+        ids =[] 
+        names = []
+        for id in productIds:
+            product = Product.objects.get(pk=id)
+            Board.objects.create(product=product)
+            ids.append(product.id) 
+            names.append(product.name) 
+        return HttpResponse(json.dumps({'id': ids, 'name': names}), content_type="application/json")
